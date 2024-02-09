4c3497ebb4d58983881ee391f189a976191c4f48
cocahonka <cocahonka@gmail.com>
2024-02-09 19:45:04 +0300

    update place model for json works

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -14,11 +14,55 @@ class Place {
     String? id,
   }) : id = id ?? DateTime.now().toIso8601String();
 
+  factory Place.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': final String id,
+        'title': final String title,
+        'address': final String address,
+        'location': {
+          'latitude': final double latitude,
+          'longitude': final double longitude,
+        },
+        'image': final String imagePath,
+      } =>
+        Place(
+          id: id,
+          title: title,
+          address: address,
+          location: (
+            latitude: latitude,
+            longitude: longitude,
+          ),
+          image: File(imagePath),
+        ),
+      _ => throw const FormatException('Failed to parse place')
+    };
+  }
+
   final String id;
   final String title;
   final String address;
   final LatLng location;
   final File image;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'address': address,
+      'location': {
+        'latitude': location.latitude,
+        'longitude': location.longitude,
+      },
+      'image': image.path,
+    };
+  }
+
+  @override
+  String toString() {
+    return toJson().toString();
+  }
 
   @override
   int get hashCode => Object.hash(id, title, address, location, image);
