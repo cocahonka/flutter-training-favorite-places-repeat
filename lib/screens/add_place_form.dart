05ca87c9960387d214a833e3609a4b07e414df35
cocahonka <cocahonka@gmail.com>
2024-02-10 11:23:46 +0300

    made up initial add place form

--- lib/screens/add_place_form.dart
+++ lib/screens/add_place_form.dart
@@ -1,13 +1,107 @@
 import 'package:flutter/material.dart';
 
-class AddPlaceForm extends StatelessWidget {
+class AddPlaceForm extends StatefulWidget {
   const AddPlaceForm({super.key});
+
+  @override
+  State<AddPlaceForm> createState() => _AddPlaceFormState();
+}
+
+class _AddPlaceFormState extends State<AddPlaceForm> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Add new Place'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 18),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 100,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  label: Text(
+                    'Title',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Container(
+                  height: 250,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                    ),
+                  ),
+                  child: TextButton.icon(
+                    label: const Text('Take Picture'),
+                    icon: const Icon(Icons.camera),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Container(
+                  height: 180,
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                    ),
+                  ),
+                  child: Text(
+                    'No location chosen',
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.place),
+                      label: const Text('Get Current Location'),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.map),
+                      label: const Text('Select on Map'),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                  label: const Text('Add Place'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
