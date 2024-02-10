4d634a70133bc8e558a1d33a1c2724fa1bce48d1
cocahonka <cocahonka@gmail.com>
2024-02-10 10:46:00 +0300

    initial place list made up

---
+++ lib/screens/add_place_form.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceForm extends StatelessWidget {
+  const AddPlaceForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new Place'),
+      ),
+    );
+  }
+}
--- lib/screens/places_screen.dart
+++ lib/screens/places_screen.dart
@@ -1,13 +1,65 @@
+import 'package:favorite_places_repeat/scopes/place_bloc.dart';
+import 'package:favorite_places_repeat/scopes/place_scope.dart';
+import 'package:favorite_places_repeat/screens/add_place_form.dart';
 import 'package:flutter/material.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends StatefulWidget {
   const PlacesScreen({super.key});
+
+  @override
+  State<PlacesScreen> createState() => _PlacesScreenState();
+}
+
+class _PlacesScreenState extends State<PlacesScreen> {
+  late final PlaceBloc _bloc;
+
+  void _openAddPlaceFormScreen() {
+    Navigator.of(context).push<void>(
+      MaterialPageRoute(
+        builder: (_) => const AddPlaceForm(),
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _bloc = PlaceScope.of(context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your places'),
+        actions: [
+          IconButton(
+            onPressed: _openAddPlaceFormScreen,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: _bloc.stream,
+        builder: (context, snapshot) {
+          return switch (snapshot.connectionState) {
+            ConnectionState.none || ConnectionState.waiting => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ConnectionState.active || ConnectionState.done => ListView.separated(
+                itemBuilder: (context, index) {
+                  final place = snapshot.data![index];
+                  return ListTile(
+                    title: Text(place.title),
+                    subtitle: Text(place.address),
+                    leading: const CircleAvatar(),
+                  );
+                },
+                separatorBuilder: (_, __) => const Divider(),
+                itemCount: snapshot.data!.length,
+              ),
+          };
+        },
       ),
     );
   }
