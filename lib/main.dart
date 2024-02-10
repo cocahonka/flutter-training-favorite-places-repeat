b3006010c801c6a24b22d99ca93fc8f8017e8cc9
cocahonka <cocahonka@gmail.com>
2024-02-10 17:35:53 +0300

    provide location bloc to the widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places_repeat/scopes/location_scope.dart';
 import 'package:favorite_places_repeat/scopes/place_scope.dart';
 import 'package:favorite_places_repeat/screens/places_screen.dart';
 import 'package:flutter/material.dart';
@@ -12,24 +13,26 @@ class FavoritePlacesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PlaceScope(
-      child: MaterialApp(
-        theme: ThemeData.dark().copyWith(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 255, 20, 120),
-            brightness: Brightness.dark,
-          ),
-          textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
-            titleLarge: GoogleFonts.ubuntuCondensed(
-              fontWeight: FontWeight.bold,
+    return LocationScope(
+      child: PlaceScope(
+        child: MaterialApp(
+          theme: ThemeData.dark().copyWith(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 255, 20, 120),
+              brightness: Brightness.dark,
             ),
-            titleMedium: GoogleFonts.ubuntuCondensed(
-              fontWeight: FontWeight.bold,
+            textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+              titleLarge: GoogleFonts.ubuntuCondensed(
+                fontWeight: FontWeight.bold,
+              ),
+              titleMedium: GoogleFonts.ubuntuCondensed(
+                fontWeight: FontWeight.bold,
+              ),
+              titleSmall: GoogleFonts.ubuntuCondensed(),
             ),
-            titleSmall: GoogleFonts.ubuntuCondensed(),
           ),
+          home: const PlacesScreen(),
         ),
-        home: const PlacesScreen(),
       ),
     );
   }
